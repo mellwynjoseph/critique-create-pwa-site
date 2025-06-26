15aeeffe3d413c2aecdd790446c28757de82e23d
mellwynjoseph <118490521+mellwynjoseph@users.noreply.github.com>
2025-06-27 04:48:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/critique-create-pwa-site/',
   server: {
     host: "::",
     port: 8080,
